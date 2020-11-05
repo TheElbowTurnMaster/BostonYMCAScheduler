670affdb5704424e0e6ea89cf778fde03ec92d2d
Jeff <Airlegoland@gmail.com>
2020-11-05 13:15:23 -0500

    Fixed improper use of imports and exports, multiple times can be selected now

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+const info = require('./personal-info.js');
 const puppeteer = require('puppeteer');
 
 Date.prototype.addDays = function(days) {
@@ -7,9 +8,13 @@ Date.prototype.addDays = function(days) {
 }
 
 async function scrapeYMCA() {
-    const time = '12:30 pm';
+    const times = ['12:30 pm','12:00 pm'];
+
+    function timeXPath(time) {
+        return '//span[text()="' + time + '"]';
+    }
     const url = 'https://outlook.office365.com/owa/calendar/HuntingtonFitnessCenter@ymcaboston.org/bookings/';
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless : true});
     const page = await browser.newPage();
     await page.goto(url);
 
@@ -22,26 +27,29 @@ async function scrapeYMCA() {
         await page.click('[class="image icon-chevronRight"]');
     }
 
-    var xpath = '//div[text()="' + dateDay + '"]';
+    let xpath = '//div[text()="' + dateDay + '"]';
     const [buttonDate] = await page.$x(xpath);
     if(buttonDate) {
         await buttonDate.click();
     }
-    
-    try {
-        const thing =  await page.waitForXPath('//span[text()="' + time + '"]', {timeout: 10000});
-     } catch(error) {
-         console.log(error);
-    }
-    
-    const [button] = await page.$x('//span[text()="' + time + '"]');
-    if(button) {
-        await button.click();
+
+    await page.waitForXPath('//div[@class="focusable timePicker"]/ul/li');
+    let availableTimeElements = await page.$x('//div[@class="focusable timePicker"]/ul/li');
+    let availableTimes = await page.evaluate((...availableTimeElements) => {
+        return availableTimeElements.map(e => e.innerText);
+    }, ...availableTimeElements);
+
+    for(let i = 0; i < availableTimes.length; i++) {
+        if(times.includes(availableTimes[i])) {
+            availableTimeElements[i].click();
+            break;
+        }
     }
 
-    await page.type('[placeholder = Name', myName);
-    await page.type('[placeholder = Email', email);
-    await page.type('[placeholder="Phone number"]', phoneNumber);
+    await page.focus('[placeholder = Name]');
+    await page.type('[placeholder = Name]', info.info.myName);
+    await page.type('[placeholder = Email]', info.info.email);
+    await page.type('[placeholder="Phone number"]', info.info.phoneNumber);
 
     await page.select('#af1aa7f3-a272-4226-8ac9-815f58267c52', "Yes");
 
